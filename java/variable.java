3af27224cca7b42e98839316e0bbd45340a19898
Satyakanta <satyakantapatra18@gmail.com>
2024-10-13 19:08:55 +0530

    java program

--- java/variable.java
+++ java/variable.java
@@ -1,22 +1,21 @@
 import java.util.*;
 
-class A
-{
-    int a ;
-    static int b = 20;
+class A {
+    int a; // Instance variable
+    static int b = 20; // Static variable
      
-    void display(int x){
-        this.a = x;
-        int var = 22;
-        System.out.println("Value of var is: "+var);
-        System.out.println(" static Value of a is: "+b);
-        System.out.println("Value of a is: "+this.a);
+    void display(int x) {
+        this.a = x; // Assigning value to instance variable
+        int var = 22; // Local variable
+        System.out.println("Value of local var is: " + var); // Printing local variable
+        System.out.println("Static Value of b is: " + b); // Printing static variable
+        System.out.println("Value of instance variable a is: " + this.a); // Printing instance variable
     }
 
-    public static void main(String a[])
-    {
-        A aa = new A();
-        a. display(10);
+    public static void main(String a[]) {
+        A aa = new A(); // Creating an object of class A
+        aa.display(10); // Calling display method
     }
 }
+
 
