f417fdf80b7538be938f7049dd4492c40224111a
Satyakanta <satyakantapatra18@gmail.com>
2024-10-13 19:04:12 +0530

    add java file

--- java/variable.java
+++ java/variable.java
@@ -6,16 +6,16 @@ class A
     static int b = 20;
      
     void display(int x){
-        a = x;
+        this.a = x;
         int var = 22;
         System.out.println("Value of var is: "+var);
         System.out.println(" static Value of a is: "+b);
-        System.out.println("Value of a is: "+a);
+        System.out.println("Value of a is: "+this.a);
     }
 
     public static void main(String a[])
     {
-        A aa = new A()
+        A aa = new A();
         a. display(10);
     }
 }
